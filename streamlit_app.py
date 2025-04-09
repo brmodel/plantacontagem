e497f659a7210614528d0d5bc2ee1c880919230d
Pedro de Freitas Pereira <defreitas.pedrop@gmail.com>
2025-04-09 16:25:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pyogrio as pyo
 import requests
 from streamlit_folium import st_folium
 import branca
-import leaflet.locatecontrol as lf_lc
+import leaflet.locatecontrol
 
 ## Configurações da Página ##
 st.set_page_config(layout="wide")
