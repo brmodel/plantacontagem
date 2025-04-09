d3aebbc6009980732d1b16591ee21c21ee1287e5
Pedro de Freitas Pereira <defreitas.pedrop@gmail.com>
2025-04-09 16:21:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,9 @@ import pyogrio as pyo
 import requests
 from streamlit_folium import st_folium
 import branca
+import "leaflet.locatecontrol"; // Import plugin
+import "leaflet.locatecontrol/dist/L.Control.Locate.min.css"; // Import styles
+import L from "leaflet"; // Import L from leaflet to start using the plugin
 
 ## Configurações da Página ##
 st.set_page_config(layout="wide")
