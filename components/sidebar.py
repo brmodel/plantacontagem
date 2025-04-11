eaaa15962fd2b9c831bb4878d8f38b85c327b113
Pedro de Freitas Pereira <defreitas.pedrop@gmail.com>
2025-04-11 11:45:18 -0300

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,6 +1,1 @@
-import streamlit as st
 
-def show_sidebar():
-    with st.sidebar:
-        st.sidebar.image("https://raw.githubusercontent.com/brmodel/plantacontagem/main/images/contagem_sem_fome.png")
-        st.header('Sidebar Menu')
