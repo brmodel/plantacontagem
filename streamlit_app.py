bbb1e4cfa8e9b4ec117150ddab396f4bd781acd2
Pedro de Freitas Pereira <defreitas.pedrop@gmail.com>
2025-04-11 12:14:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ df = load_data()
 regionais_json = load_geojson()
 
 # --- Create Folium Map ---
-m = folium.Map(location=[-19.89323, -44.00145], zoom_start=12.54)
+m = folium.Map(location=[-19.89323, -44.00145], zoom_start=12.48)
 
 # Add GeoJSON layer under markers
 folium.GeoJson(
