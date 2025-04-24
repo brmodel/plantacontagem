a579b3f4b1135e00014ef35532618bac58f8d1f9
Pedro de Freitas Pereira <defreitas.pedrop@gmail.com>
2025-04-24 16:25:43 -0300

    update_v.0.8.2

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ POPUP_TEMPLATE_BASE = """
 """
 
 TOOLTIP_TEMPLATE = """
-<div style="font-family: Arial, sans-serif; font-size: 12px">
+<div style="font-family: Arial, sans-serif; font-size: 14px">
     <p><b>Unidade Produtiva:<br>{}</b></p>
 </div>
 """
