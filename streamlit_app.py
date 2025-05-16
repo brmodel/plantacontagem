f64c78015a7d9f796e0506da1dda5f573c2092e0
Pedro de Freitas Pereira <defreitas.pedrop@gmail.com>
2025-05-16 15:38:52 -0300

    update_v.0.8.8

--- streamlit_app.py
+++ streamlit_app.py
@@ -221,10 +221,7 @@ def main():
         
     col1, col2 = st.columns([3, 1])
     with col1:
-        st.title(APP_TITULO)
-        logo_bytes = get_image_bytes(LOGO_PMC_URL_CABEÇALHO)
-        if logo_bytes: st.image(logo_bytes, width=150)
-        else: st.image(LOGO_PMC_URL_CABEÇALHO, width=150);
+        st.title(APP_TITULO);
         st.header(APP_SUBTITULO)
     with col2:
         # Usa LOGO_PMC_URL_CABEÇALHO para o logo no cabeçalho
